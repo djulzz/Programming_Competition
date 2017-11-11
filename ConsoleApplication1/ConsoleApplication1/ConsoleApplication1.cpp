4974c441554cc209b34217845712e7c4286d9454
bamaisfunny <theantman15@yahoo.com>
2017-11-10 21:56:07 -0800

    ANTHONY
    ANTHONY

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	int num = 0;
+	std::string name;
+
+	std::cin >> num;
+	std::cin >> name;
+
+	std::cout << num << std::endl;
+	std::cout << name << std::endl;
+
+	system("PAUSE");
+
+    return 0;
+}
+
